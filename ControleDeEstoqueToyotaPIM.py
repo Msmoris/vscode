8476499a311a9b151a061b729d441d562cd76207
bk <marcellesmoris@gmail.com>
2026-05-09 23:08:18 -0300

    deu rui:(

--- ControleDeEstoqueToyotaPIM.py
+++ ControleDeEstoqueToyotaPIM.py
@@ -5,12 +5,47 @@
 pecas = []
 
 def cadastrar_peca():
-    nome = input('Digite o nome da peça:')
+    nome = input('Digite o nome da peça: ')
     quantidade = int(input('Digite a quantidade de peças: '))
 
     peca = {
         'nome': nome,
         'quantidade': quantidade
     }
-
+    
     pecas.append(peca) 
+
+    print('peça cadastrada com sucesso!')
+
+# Função para mostrar estoque
+def mostrar_estoque():
+    if len(pecas) == 0:
+        print('Nenhuma peça cadastrada.')
+    else:
+        print('\nESTOQUE DA TOYOTA')
+
+        for peca in pecas:
+            print(f'Peça: {peca['nome']}')
+            print(f'Quantidade: {peca['quantidade']}')
+            print('-' * 20)
+
+# Menu principal
+    while True:
+        print('\nSISTEMA TOYOTA')
+        print('1 - Cadastrar peça')
+        print('2 - Mostrar estoque')
+        print('3 - Sair')
+
+        opcao = input('Escolha uma opção: ')
+
+        if opcao == '1':
+            cadastrar_peca()
+        elif opcao == '2':
+            mostrar_estoque()
+        elif opcao == '3':
+            print('Sistema encerrado.')
+            break
+        else:
+            print('Opção invalida.')            
+
+
