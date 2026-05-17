8e28ea9d9e0fde9b63d26feb0f0f5521b95b92f3
bk <marcellesmoris@gmail.com>
2026-05-17 15:13:33 -0300

    right system

--- sistemaDeDistribuicaoDeOleo.py
+++ sistemaDeDistribuicaoDeOleo.py
@@ -1,3 +1,38 @@
-# Sistema de Distribuição de Oleo - Toyota Boshoku
+# Reservatório inicial
+reservatorio = 0
 
-# Reservatório Inicial
+# Função para abastecer o reservatório
+
+# Cria uma função
+def abastecer_reservatorio():
+
+    global reservatorio
+
+    litros = float(input('Digite a quantidade de óleo adicionada: '))
+
+    reservatorio += litros
+
+    print(f'Reservatório atualizado: {reservatorio} litros')
+
+    
+    # Função para distribuir o oleo
+
+    # Cria uma função
+    def distribuir_oleo():
+        
+        global reservatorio
+
+        vaga = input('Digite o numero da vaga: ')
+        quantidade = float(input('Digite a quantidade de oleo desejada: '))
+        
+    # Verifica se há óleo suficiente no reservatorio
+        if quantidade <= reservatorio:
+
+            reservatorio -= quantidade 
+
+            print('Óleo distribuido para a vaga {vaga}.')
+
+            print(f'Reservatório restante: {reservatorio} litros')
+        
+        else:
+            print('\nERRO: óleo insuficiente noo reservatório!')
