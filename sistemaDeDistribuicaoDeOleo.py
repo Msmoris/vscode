0ac6a889453dc0283202a2fa59eb899fd10460c5
bk <marcellesmoris@gmail.com>
2026-05-17 15:51:06 -0300

    right system 2.0

--- sistemaDeDistribuicaoDeOleo.py
+++ sistemaDeDistribuicaoDeOleo.py
@@ -18,21 +18,59 @@ def abastecer_reservatorio():
     # Função para distribuir o oleo
 
     # Cria uma função
-    def distribuir_oleo():
+def distribuir_oleo():
         
-        global reservatorio
+    global reservatorio
 
-        vaga = input('Digite o numero da vaga: ')
-        quantidade = float(input('Digite a quantidade de oleo desejada: '))
+    vaga = input('Digite o numero da vaga: ')
+    quantidade = float(input('Digite a quantidade de oleo desejada: '))
         
-    # Verifica se há óleo suficiente no reservatorio
-        if quantidade <= reservatorio:
+# Verifica se há óleo suficiente no reservatorio
+    if quantidade <= reservatorio:
 
-            reservatorio -= quantidade 
+        reservatorio -= quantidade 
 
-            print('Óleo distribuido para a vaga {vaga}.')
+        print('\nÓleo distribuido para a vaga {vaga}.')
 
-            print(f'Reservatório restante: {reservatorio} litros')
+        print(f'Reservatório restante: {reservatorio} litros')
         
-        else:
-            print('\nERRO: óleo insuficiente noo reservatório!')
+    else:
+        print('\nERRO: óleo insuficiente noo reservatório!')
+
+# Função para mostar reservatório
+def mostar_reservatorio():
+
+    print(f'\nQuantidade atual no reservatório: {reservatorio} litros')
+
+    print('='*10)
+
+
+# Menu Principal
+
+while True:
+
+    print('\nRESERVATÓRIO DE ÓLEO')
+    print('1 - Abastecer reservatório')
+    print('2 - Distribuir óleo')
+    print('3 - Mostrar reservatório')
+    print('4 - Sair')
+
+    opcao = input('Escolha uma opção: ')
+
+    if opcao == '1':
+        abastecer_reservatorio()
+
+    elif opcao == '2':
+        distribuir_oleo()
+
+    elif opcao == '3':
+        mostar_reservatorio()
+
+    elif opcao == '4':
+        print('\nSistema encerrado.')
+        break
+
+    else:
+        print('\nOpção inválida. Tente novamente.')
+
+        
