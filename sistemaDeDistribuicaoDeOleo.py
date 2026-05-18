9c3a3a774cdcf57d5204b46fe29cd4a35042f5c8
bk <marcellesmoris@gmail.com>
2026-05-17 23:31:12 -0300

    socorro

--- sistemaDeDistribuicaoDeOleo.py
+++ sistemaDeDistribuicaoDeOleo.py
@@ -1,5 +1,6 @@
 # Reservatório inicial
 reservatorio = 0
+relatorio = []
 
 # Função para abastecer o reservatório
 
@@ -35,6 +36,25 @@ def distribuir_oleo():
 
         reservatorio -= quantidade 
 
+    dados = {
+        'vaga': vaga,
+        'quantidade': quantidade
+    }
+
+    relatorio.append(dados)
+    
+    if quantidade <= reservatorio:
+
+        reservatorio -= quantidade
+
+        #  Guarda informações no relatorio
+        dados = {
+            'vaga': vaga,
+            'quantidade':quantidade
+        } 
+
+        relatorio.append(dados)
+
         print(f'\nÓleo distribuido para a vaga {vaga}.')
         # Mostra a vaga escolhida pelo usuario
 
@@ -42,6 +62,26 @@ def distribuir_oleo():
         # Mostra a quantidade restante do reservatorio
     else:
         print('\nERRO: óleo insuficiente no reservatório!')
+
+def mostrar_relatorio():
+    if len(relatorio) == 0:
+        print('\nNenhum consumo registrado')
+    else:
+        print('\nRELATORIO DE CONSUMO')
+
+        total = 0
+
+        for item in relatorio:
+
+            print(f'Vaga: {item["vaga"]}')
+            print(f'Quantidade consumida: {item["quantidade"]} litros')
+
+            total += item['quantidade']
+
+        print('=' * 20)
+
+    print(f"\nConsumo total: {total} litros.")
+
 
 # Função para mostar reservatório
 def mostar_reservatorio():
@@ -60,6 +100,7 @@ def mostar_reservatorio():
     print('='*50)
 
 
+
 # Menu Principal
 
 while True:
@@ -68,7 +109,8 @@ while True:
     print('1 - Abastecer reservatório')
     print('2 - Distribuir óleo')
     print('3 - Mostrar reservatório')
-    print('4 - Sair')
+    print('4 - Mostrar relatório')
+    print('5 - Sair')
 
     opcao = input('Escolha uma opção: ')
 
@@ -82,9 +124,13 @@ while True:
         mostar_reservatorio()
 
     elif opcao == '4':
-        print('\nSistema encerrado.')
-        break
-        # O 'break' encerra o sistema
+       mostrar_relatorio()
+
+    elif opcao == '5':
+      print('\nSistema encerrado.')
+      break
+      # O 'break' encerra o sistema
+
     else:
         print('\nOpção inválida. Tente novamente.')
 
