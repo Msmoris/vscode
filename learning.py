4946be2094b9bba9115a24a2d07fdb76225d0cda
bk <marcellesmoris@gmail.com>
2026-06-03 22:35:45 -0300

    inicio

---
+++ learning.py
@@ -0,0 +1,13 @@
+
+
+# lista1 = []
+# idade = int(input('qual sua idade?: '))
+# # .append() adiciona item na lista
+# lista1.append(idade)
+# print(lista1)
+
+lista2 = ['',2,4,'va se lasca',5,90,'pqp']
+print(lista2)
+
+while True:
+
